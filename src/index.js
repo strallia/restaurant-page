f8da150de143135845964f87a7231ce0aae27a10
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-01-12 13:03:17 -0800

    Render page content based on clicked tab

--- src/index.js
+++ src/index.js
@@ -5,6 +5,29 @@ import './styles.css';
 
 const container = document.querySelector('div#content');
 
-renderHome(container);
-renderMenu(container);
-renderContact(container);
+const tabs = document.querySelectorAll('nav > button');
+tabs.forEach((tab) => {
+  tab.addEventListener('click', (event) => renderContent(event.target));
+})
+
+function renderContent(activeTab) {
+  // render active tab
+  tabs.forEach((tab) => {
+    tab.setAttribute('data-status', 'inactive');
+  });
+  activeTab.setAttribute('data-status', 'active');
+
+  // render activeTab's content
+  container.textContent = '';
+  const tabValue = activeTab.getAttribute('data-value');
+  switch(tabValue) {
+    case 'home':
+      renderHome(container);
+      break;
+    case 'menu':
+      renderMenu(container);
+      break;
+    case 'contact':
+      renderContact(container);
+  }; 
+}
