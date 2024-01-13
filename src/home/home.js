d706c32e11aae88798b088c75bc76930101732f8
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-01-12 21:43:05 -0800

    Style home page content

--- src/home/home.js
+++ src/home/home.js
@@ -1,16 +1,62 @@
 import Icon from './icon.png';
 
 export default function (container) {
+  // welcome section
+  const welcomeDiv = document.createElement('div');
+  const imgDiv = document.createElement('div');
   const img = document.createElement('img');
+  const textDiv = document.createElement('div');
   const h1 = document.createElement('h1');
-  const para = document.createElement('p');
-  
-  img.src = Icon;
-  img.classList.add('img');
-  h1.textContent = 'Home';
-  para.textContent = 'some random text';
+  const welcomePara = document.createElement('p');
 
-  container.appendChild(img);
-  container.appendChild(h1);
-  container.appendChild(para);
+  welcomeDiv.classList.add('welcome');
+  img.src = Icon;
+  h1.textContent = 'Welcome to Kitty Cafe!';
+  welcomePara.textContent= "The ultimate destination for cat lovers and food enthusiasts. Our Kitty Cafe offers a purr-fect haven for relaxation and feline companionship.";
+
+  imgDiv.appendChild(img);
+  [h1, welcomePara].forEach((element) => textDiv.appendChild(element));
+  [imgDiv, textDiv].forEach((element) => welcomeDiv.appendChild(element));
+
+
+  // details section
+  const bubbleDiv = document.createElement('div');
+  const borderDiv = document.createElement('div');
+  bubbleDiv.classList.add('bubble-section');
+
+  const details = [
+    {
+      title: 'Open Hours',
+      text: [
+        "Monday - Friday : 10am - 6pm",
+        "Saturday - Sunday : 10am - 8pm"
+      ]
+    },
+    {
+      title: 'Location',
+      text: [
+        "111 Sunshine Lane",
+        "Neverland, CA"
+      ]
+    }
+  ];
+  details.forEach((section) => {
+    const h2 = document.createElement('h2');
+    h2.textContent = section.title;
+
+    const listDiv = document.createElement('div');
+    const textArr = section.text;
+    textArr.forEach((string) => {
+      const para = document.createElement('p');
+      para.textContent = string;
+      listDiv.appendChild(para);
+    });
+
+    [h2, listDiv].forEach((element) => borderDiv.appendChild(element));
+  });
+  bubbleDiv.appendChild(borderDiv);
+
+
+  container.appendChild(welcomeDiv);
+  container.appendChild(bubbleDiv);
 };
